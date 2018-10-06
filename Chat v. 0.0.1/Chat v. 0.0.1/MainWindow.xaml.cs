77d65829d72455d288cb910a3ed1c8245fb83edb
DeadRipper <hardyextrim@gmail.com>
2018-10-06 15:45:52 +0300

    Add files via upload

---
+++ Chat v. 0.0.1/Chat v. 0.0.1/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat_v._0._0._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static string userName;
+        //public string host = ip_textbox.Text;
+        //public int port = port_textbox.Text;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        public static void Work()
+        {
+            Console.Write("Введите свое имя: ");
+            userName = Console.ReadLine();
+            client = new TcpClient();
+            try
+            {
+                //if(client.Client.Disconnect = false)
+                client.Connect(ip_textbox.Text, port_textbox.Text); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                // запускаем новый поток для получения данных
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start(); //старт потока
+                Console.WriteLine("Добро пожаловать, {0}", userName);
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        // отправка сообщений
+        static void SendMessage()
+        {
+            Console.WriteLine("Введите сообщение: ");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        // получение сообщений
+        static void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message); //вывод сообщения
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close(); //отключение потока
+            if (client != null)
+                client.Close(); //отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
+
+
+        private void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            ServerObject chat = new ServerObject();
+            Thread ListenThread = new Thread(new ThreadStart(chat.Listen));
+            ListenThread.Start();
+            string data = TextBox.Text.ToString();
+            chat.
+            data = MainTextBlock.Text;
+        }
+    }
+
+}
